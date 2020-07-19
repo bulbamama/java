8f8ada39628be4bf4972cb924ef95ee4deb667ec
bulbamama <bulbaf@bk.ru>
2020-07-19 22:59:34 +0300

    Добавил несколько функций

--- testBox/src/main/java/ru/stqa/pft/testbox/program.java
+++ testBox/src/main/java/ru/stqa/pft/testbox/program.java
@@ -2,8 +2,25 @@ package ru.stqa.pft.testbox;
 
 public class program {
 	public static void main(String[] args) {
-	System.out.println("Hello Hello Hello");
-	
+
+	hello("user");
+	hello("Alex");
+	hello("Gabe");
+
+	double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " ровна " + area(l));
+
+	double a = 4;
+	double b = 6;
+		System.out.println("Прямоугольник со сторонами " + "ровна" + area(a, b));
+
 	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+	public static double area(double l) {return l*l; }
+	public static double area(double a, double b) {return a * b; }
+	
 
 }
