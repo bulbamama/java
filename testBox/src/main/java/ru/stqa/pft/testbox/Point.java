4b160b0de5dbb38349d5c0bd77dea19305248e08
bulbamama <bulbaf@bk.ru>
2020-07-23 22:08:04 +0300

    Перенес метод distance в класс Point

--- testBox/src/main/java/ru/stqa/pft/testbox/Point.java
+++ testBox/src/main/java/ru/stqa/pft/testbox/Point.java
@@ -3,15 +3,13 @@ package ru.stqa.pft.testbox;
 public class Point {
     public double x;
     public double y;
-/*
+
 
     public Point(double x, double y) {
         this.x=x;
         this.y=y;
     }
-    public double distance() {
-        return Math.sqrt((this.x-this.x)*(this.x-this.x)+(this.y-this.y)*(this.y-this.y));
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
     }
-
-*/
 }
