cde1d32e1db317d67e43df295f9f7ad6a3953b75
bulbamama <bulbaf@bk.ru>
2020-07-25 12:44:38 +0300

    Написал тест по заданию №3

---
+++ testBox/src/test/java/ru/stqa/pft/testbox/PointTest.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.testbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+    @Test
+    public void TestDistance() {
+        Point p1 = new Point(10, 10);
+        Point p2 = new Point(0, 0);
+        Assert.assertEquals(Point.distance(p1, p2), 14.142135623730951);
+
+    }
+}
