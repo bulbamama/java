24b2c43cbccfea383623e42546cf6076c26860b9
bulbamama <bulbaf@bk.ru>
2020-07-30 00:14:59 +0300

    Создание контакта
    -сделал рефакторинг
    -добавил вспомагательный класс ContactData

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firsname;
+    private final String lastname;
+    private final String address;
+    private final String phonehome;
+    private final String email;
+
+    public ContactData(String firsname, String lastname, String address, String phonehome, String email) {
+        this.firsname = firsname;
+        this.lastname = lastname;
+        this.address = address;
+        this.phonehome = phonehome;
+        this.email = email;
+    }
+
+    public String getFirsname() {
+        return firsname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhonehome() {
+        return phonehome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
