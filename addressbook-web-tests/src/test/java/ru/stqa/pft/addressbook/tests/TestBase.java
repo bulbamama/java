e4902114bee4f0a5902c1cf0d6a0738413016d36
bulbamama <bulbaf@bk.ru>
2020-08-02 23:35:23 +0300

    TestBase
    Рабочий класс TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import ru.stqa.pft.addressbook.model.ContactData;
 
 import static org.testng.Assert.assertTrue;
 
@@ -28,40 +27,6 @@ public class TestBase {
       wd.findElement(By.name("pass")).clear();
       wd.findElement(By.name("pass")).sendKeys(password);
       wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
-    public void logout() {
-      wd.findElement(By.linkText("Logout")).click();
-    }
-
-    public void gotoHomePage() {
-      wd.findElement(By.linkText("home page")).click();
-    }
-
-    public void submitContact() {
-      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    }
-
-    public void fillFieldsContact(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirsname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.getPhonehome());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    public void gotoAddNewContact() {
-      wd.findElement(By.linkText("add new")).click();
     }
 
     public void deleteContact() {
@@ -93,4 +58,5 @@ public class TestBase {
         acceptNextAlert = true;
       }
     }
+    
 }
