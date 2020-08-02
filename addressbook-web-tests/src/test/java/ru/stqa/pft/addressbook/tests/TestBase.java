34fbc0ee01eb5d2cf8a18ae70acd74a0aedbc854
bulbamama <bulbaf@bk.ru>
2020-08-02 23:49:37 +0300

    TestBaseV0.2
    Работающий класс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -20,15 +20,6 @@ public class TestBase {
         app.init();
     }
 
-    public void login(String username, String password) {
-      wd.findElement(By.name("user")).click();
-      wd.findElement(By.name("user")).clear();
-      wd.findElement(By.name("user")).sendKeys(username);
-      wd.findElement(By.name("pass")).clear();
-      wd.findElement(By.name("pass")).sendKeys(password);
-      wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
-
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
