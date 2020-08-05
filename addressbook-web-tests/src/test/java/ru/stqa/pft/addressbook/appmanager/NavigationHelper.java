e73f06b6b96aaf6d37854419eca19224e206cf86
bulbamama <bulbaf@bk.ru>
2020-08-05 22:45:52 +0300

    Исправления по классу NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,9 +5,6 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
-    public WebDriver wd;
-    public boolean acceptNextAlert = true;
-
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
