f5c41097b27e16db64dc50ff617d7beb8f2d5e91
bulbamama <bulbaf@bk.ru>
2020-08-07 23:43:36 +0300

    Запуск тестов из разных браузеров
    IE & FIREFOX запускаются без ошибок
    при запуске теста в CHROME браузер открывается, но тест запускается в FIREFOX

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,11 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.assertTrue;
 
 public class ApplicationManager {
 
@@ -17,8 +17,20 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+        if(browser == BrowserType.FIREFOX)  {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME)   {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE)   {
+            wd = new InternetExplorerDriver();
+        }
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
